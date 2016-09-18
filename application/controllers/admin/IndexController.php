d9f0e2dc216f47a2d73b89e91ba86d0da665934f
Hsu <xusasuke6@gmail.com>
2016-09-18 08:08:53 +0800

    add the backend view

--- application/controllers/admin/IndexController.php
+++ application/controllers/admin/IndexController.php
@@ -4,9 +4,25 @@
 class IndexController
 {
     public function indexAction() {
-//       echo "admin....index";
 
         include CUR_VIEW_PATH . "index.html";
 
     }
+
+    public function topAction() {
+        include CUR_VIEW_PATH . "top.html";
+    }
+
+    public function menuAction() {
+        include CUR_VIEW_PATH . "menu.html";
+    }
+
+    public function dragAction() {
+        include CUR_VIEW_PATH . "drag.html";
+    }
+
+    public function mainAction() {
+        include CUR_VIEW_PATH . "main.html";
+    }
+
 }
